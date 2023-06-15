aab423492875545c43c6cf58d0b2ddbd3aaeea64
dory1110 <dory.grossman@digitalturbine.com>
2023-06-15 17:38:30 +0300

    kmeans api

--- kmeansmodule.c
+++ kmeansmodule.c
@@ -7,14 +7,57 @@ static PyObject* fit(PyObject *self, PyObject *args){
     int K, iter;
     int numberOfvectors, vectorsLength;
     double eps;
-    double** vectorsList;
-    double** centeroids;
+    PyObject vectorsList;
+    PyObject centeroids;
 
-    if(!PyArg_ParseTuple(args, "di", &K, &iter, &numberOfvectors, &vectorsList, &eps, )) {
+    double **vectors;
+    double **centers;
+    double **finalCenteroids;
+
+    PyObject* pyMatrix;
+
+
+    if(!PyArg_ParseTuple(args, "iiiidoo", &K, &iter, &numberOfvectors, &vectorsLength, &eps, &vectorsList, &centeroids)) {
         return NULL; 
     }
 
+    vectors = convertPyMatToCMat(vectorsList, numberOfvectors, vectorsLength);
+    centers = convertPyMatToCMat(centeroids, K, vectorsLength);
 
+    finalCenteroids = kMeans(K, iter, numberOfvectors, vectorsLength, eps, vectors, centers);
+    
+    pyMatrix = Py_BuildValue("[");
+    for (int i = 0; i < rows; i++) {
+        PyObject* pyRow = Py_BuildValue("[");
+        for (int j = 0; j < cols; j++) {
+            PyObject* pyValue = Py_BuildValue("d", cMatrix[i][j]);
+            PyList_Append(pyRow, pyValue);
+            Py_DECREF(pyValue);
+        }
+        PyList_Append(pyMatrix, pyRow);
+        Py_DECREF(pyRow);
+    }
+    return pyMatrix;
+}
+
+double** convertPyMatToCMat(pyObject matrix, int row, int col){
+    int i;
+    int j;
+    double** mat;
+    PyObject* rowPy;
+    PyObject* item;
+
+    mat = (double**) malloc(row * sizeof(double*));
+    for (i = 0; i < row; i++) {
+        mat[i] = (double*)malloc(col * sizeof(double));
+
+        rowPy = PyList_GetItem(matrix, i);
+        for (j = 0; j < col; j++) {
+            item = PyList_GetItem(rowPy, j);
+            vectors[i][j] = PyLong_AsLong(item);  // Assuming matrix elements are integers
+        }
+    }
+    return mat;
 
 }
 
