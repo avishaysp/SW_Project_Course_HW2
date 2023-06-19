6f384ed70db75e4d4a0901fd823289e4d34393f4
dory1110 <dory.grossman@digitalturbine.com>
2023-06-19 17:17:37 +0300

    .

--- kmeansmodule2.c
+++ kmeansmodule2.c
@@ -1,6 +1,26 @@
 # define PY_SSIZE_T_CLEAN
 # include <Python.h>
-# include "kmeans.c"
+# include "kmeans.h"
+
+static void fit(){
+    printf("%s", fit);
+}
+
+static PyMethodDef kmeansMethods[] = {
+    {"fit",
+      (PyCFunction) fit,
+      METH_VARARGS,           
+      PyDoc_STR("Calculate the K centeroid of list of vector with max number of iteraion iter")}, 
+    {NULL, NULL, 0, NULL}     
+};
+
+static struct PyModuleDef kmeansmodule = {
+    PyModuleDef_HEAD_INIT,
+    "mykmeanssp",
+    NULL, 
+    -1,  
+    kmeansMethods 
+};
 
 PyMODINIT_FUNC PyInit_mykmeanssp(void)
 {
