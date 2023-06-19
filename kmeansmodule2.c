c794e2e09e8cf11881d33aca4c0ea9104c208c88
dory1110 <dory.grossman@digitalturbine.com>
2023-06-19 17:13:58 +0300

    .

--- kmeansmodule2.c
+++ kmeansmodule2.c
@@ -2,3 +2,12 @@
 # include <Python.h>
 # include "kmeans.c"
 
+PyMODINIT_FUNC PyInit_mykmeanssp(void)
+{
+    PyObject *m;
+    m = PyModule_Create(&kmeansmodule);
+    if (!m) {
+        return NULL;
+    }
+    return m;
+}
